0f2f909ab109ea89f773ed954d9999205eb75fbd
Alfonso Blanco <alfonsoblanco5050@gmail.com>
2021-11-09 19:13:07 +0100

    Add files via upload

---
+++ MNIST_KNN_sklearn_with_test_out_train.py
@@ -0,0 +1,121 @@
+import numpy as np
+import pandas as pd
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+arr=[]
+arry=[]
+Start=1
+ContaMax=100;
+f=open("C:\\mnist_test.csv","r")
+
+Conta=0;
+for linea in f:
+    Conta=Conta+1
+    if Conta<=Start: continue
+    if Conta > ContaMax :break
+    lineadelTrain =linea.split(",")
+   
+ 
+    linea_x =[""]
+    z=0
+    for x in lineadelTrain:
+       
+        z=z+1
+        if z==785: break
+        if z==1: linea_x[0]=int(lineadelTrain[z])
+        else:  linea_x.append(int(lineadelTrain[z]))
+  
+    arr.append(linea_x)
+    arry.append(lineadelTrain[0])
+     
+
+X_test=np.array(arr)
+
+Y_test=np.array(arry)
+
+ContaMax=60000;
+f=open("C:\\mnist_train.csv","r")
+
+Conta=0;
+for linea in f:
+    Conta=Conta+1
+    if Conta==1: continue
+    if Conta > ContaMax :break
+    lineadelTrain =linea.split(",")
+  
+ 
+    linea_x =[""]
+    z=0
+    for x in lineadelTrain:
+   
+        z=z+1
+        if z==785: break
+        if z==1: linea_x[0]=int(lineadelTrain[z])
+        else:  linea_x.append(int(lineadelTrain[z]))
+  
+    arr.append(linea_x)
+    arry.append(lineadelTrain[0])
+    
+
+x=np.array(arr)
+
+y=np.array(arry)
+X_train=np.array(arr)
+
+Y_train=np.array(arry)
+
+n_train, n_test = len(X_train), len(X_test)
+   
+Y_predict, pred_test = [np.zeros(n_train), np.zeros(n_test)]
+#######################################################################333
+#KNN NEIGHBOR
+######################################################################333
+model = KNeighborsClassifier(n_neighbors=3)
+
+model.fit(X_train, Y_train)
+
+TotAciertos=0.0
+TotFallos=0.0
+
+  
+Y_predict_test=model.predict(X_test)
+
+Y_test_arr=np.array(Y_test)
+
+TotAciertos=0.0
+TotFallos=0.0
+
+
+i=1   
+for i in range (len(Y_predict_test)):
+    
+    Cadena=""
+    ContColumna=0
+    
+    for j in range (783):
+                    
+       if int(X_test[i,j+1]) <= 90:
+          Cadena=Cadena + " "
+       else:
+          Cadena=Cadena + "*"
+       ContColumna=ContColumna +1
+       if ContColumna== 28:
+             print(Cadena)
+             Cadena=""
+             ContColumna=0  
+    print ("Predicted class= "+ str(Y_predict_test[i]) + " True class ="
+             +  str(Y_test_arr[i]))
+    if (Y_predict_test[i]==Y_test_arr[i]):
+        TotAciertos=TotAciertos+1
+    else:
+        TotFallos =TotFallos + 1
+        
+print("")  
+print("RESULTS FOR KNN")     
+print("Total Hits TEST = " + str(TotAciertos))
+print("Total Failures TEST = " + str(TotFallos))
+
+
+
